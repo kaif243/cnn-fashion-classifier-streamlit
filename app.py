f3f6eb5c2a2902bef6d36fb2cb3dfb5c04cde4e9
kaif243 <kaif24sm@gmail.com>
2026-05-30 14:36:00 +0530

    Removed broken saved model

--- app.py
+++ app.py
@@ -1,52 +1,56 @@
 import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
-from tensorflow.keras.models import load_model
 from tensorflow.keras.datasets import fashion_mnist
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 
 st.set_page_config(page_title="Fashion Classification using CNN")
 
 st.title("Fashion Classification using CNN")
 
-# Load model
-model = load_model("model/cnn_model.keras", compile=False)
-
 # Load dataset
 (X_train, y_train), (X_test, y_test) = fashion_mnist.load_data()
 
+X_train = X_train.reshape(-1, 28, 28, 1) / 255.0
 X_test = X_test.reshape(-1, 28, 28, 1) / 255.0
 
-# Manual accuracy calculation
-predictions = model.predict(X_test[:1000], verbose=0)
-predicted_classes = np.argmax(predictions, axis=1)
+# Build model
+model = Sequential([
+    Conv2D(32, (3,3), activation='relu', input_shape=(28,28,1)),
+    MaxPooling2D(2,2),
+    Flatten(),
+    Dense(128, activation='relu'),
+    Dense(10, activation='softmax')
+])
 
-accuracy = np.mean(predicted_classes == y_test[:1000])
+model.compile(
+    optimizer='adam',
+    loss='sparse_categorical_crossentropy',
+    metrics=['accuracy']
+)
+
+# Train quickly
+model.fit(X_train[:5000], y_train[:5000], epochs=2, verbose=0)
+
+# Accuracy
+loss, accuracy = model.evaluate(X_test[:1000], y_test[:1000], verbose=0)
 
 st.success(f"Model Accuracy: {accuracy:.4f}")
 
 class_names = [
-    "T-shirt/Top",
-    "Trouser",
-    "Pullover",
-    "Dress",
-    "Coat",
-    "Sandal",
-    "Shirt",
-    "Sneaker",
-    "Bag",
-    "Ankle Boot"
+    "T-shirt/Top","Trouser","Pullover","Dress","Coat",
+    "Sandal","Shirt","Sneaker","Bag","Ankle Boot"
 ]
 
-# Random prediction
 idx = np.random.randint(0, len(X_test))
 sample = X_test[idx]
 
-prediction = model.predict(sample.reshape(1, 28, 28, 1), verbose=0)
+prediction = model.predict(sample.reshape(1,28,28,1), verbose=0)
 predicted_class = np.argmax(prediction)
 
-st.subheader("Prediction Result")
 st.write(f"Predicted Class: **{class_names[predicted_class]}**")
 
 fig, ax = plt.subplots()
-ax.imshow(sample.reshape(28, 28), cmap="gray")
+ax.imshow(sample.reshape(28,28), cmap="gray")
 st.pyplot(fig)
